c6e2b11af145d4c1abb7b7ef7271f648f584c828
Joao <joaomarcio.ribeiro@gmail.com>
2021-05-12 19:42:46 -0300

    Create backgroundColor.js

---
+++ backgroundColor.js
@@ -0,0 +1,6 @@
+function backgroundColor() {
+    var color = document.getElementById("inputcolor").value;
+    document.body.style.backgroundColor = color;
+    document.getElementById("inputcolor").value = "#";
+
+}
